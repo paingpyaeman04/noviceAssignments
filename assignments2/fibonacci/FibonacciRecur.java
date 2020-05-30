246bc2cd94d1c95bf33c6aab109f2eca11d0710c
paingpyaeman04 <62030108+paingpyaeman04@users.noreply.github.com>
2020-05-30 08:15:03 +0630

    Update FibonacciRecur.java

--- assignments2/fibonacci/FibonacciRecur.java
+++ assignments2/fibonacci/FibonacciRecur.java
@@ -12,7 +12,7 @@ class FibonacciRecur
 		
 		int sum = 0;
 		
-		for(int i = 1; i <= n; i++) {
+		for(int i = 0; i < n; i++) {
 			int fiboTerm = fib(i);
 			sum += fiboTerm;
 			System.out.print(fiboTerm + " ");
@@ -26,10 +26,8 @@ class FibonacciRecur
 	}
 	
 	private static int fib(int n) {
-		if(n == 1)
-			return 0;
-		if(n == 2 || n == 3)
-			return 1;
+		if(n == 0 || n == 1)
+			return n;
 		else 
 			return fib(n-2) + fib(n-1);
 	}
